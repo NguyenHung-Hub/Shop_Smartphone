1d9f87a022b19741ddd403e07e75cb732dc362ed
HUNG NGUYEN <daminhnguyenhung@gmail.com>
2021-05-29 00:52:53 +0700

    add phone.html, add data.js, nav_index.js

---
+++ assets/js/data.js
@@ -0,0 +1,214 @@
+export const Iphone = [
+    {
+        product_name: 'Iphone 12 Pro Max 128GB',
+        price: '30.990.000',
+        img: './assets/img/product/phone/iphone/iphone-12-pro-max-xanh-duong.jpg'
+    },
+    {
+        product_name: 'Iphone 12 64GB',
+        price: '20.390.000',
+        img: './assets/img/product/phone/iphone/iphone-12-violet.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Pro Max 512GB',
+        price: '41.490.000',
+        img: './assets/img/product/phone/iphone/iphone-12-pro-max-vang.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Pro Max 256GB',
+        price: '34.990.000',
+        img: './assets/img/product/phone/iphone/iphone-12-pro-max-vang.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Mini 256GB',
+        price: '21.490.000',
+        img: './assets/img/product/phone/iphone/iphone-23-mini-xanh-la-new-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Pro 256GB',
+        price: '25.690.000',
+        img: './assets/img/product/phone/iphone/iphone-12-do-new-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Mini 64GB',
+        price: '17.390.000',
+        img: './assets/img/product/phone/iphone/iphone-12-violet.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Mini 64GB',
+        price: '18.190.000',
+        img: './assets/img/product/phone/iphone/iphone-12-pro-max-xanh-duong.jpg'
+    },
+    {
+        product_name: 'Iphone 12 Pro Max 256GB',
+        price: '34.990.000',
+        img: './assets/img/product/phone/iphone/iphone-12-do-new-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'Iphone XR 128GB',
+        price: '15.590.000',
+        img: './assets/img/product/phone/iphone/iphone-xr-128gb-600x600.jpg'
+    },
+    
+];
+
+export const phone_Samsung = [
+    {
+        product_name: 'Samsung Galaxy S21 5G',
+        price: '14.990.000',
+        img: './assets/img/product/phone/samsung/samsung-galaxy-s21-violet.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Note20 Ultral 5G',
+        price: '32.990.000',
+        img: './assets/img/product/phone/samsung/sam-sung-note-20-ultra-vang-dong-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Note20 Ultral',
+        price: '29.990.000',
+        img: './assets/img/product/phone/samsung/samsung-galaxy-note-20-ultra-den-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy S21 Ultral 5G 256GB',
+        price: '25.990.000',
+        img: './assets/img/product/phone/samsung/samsung-galaxy-s21-ultra-256gb-den-600x600-1-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy S21 Ultral 5G 128GB',
+        price: '22.590.000',
+        img: './assets/img/product/phone/samsung/samsung-galaxy-s21-ultra-bac-600x600-1-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Z Fold2 5G',
+        price: '50.000.000',
+        img: './assets/img/product/phone/samsung/samsung-galaxy-z-fold-2-vang-dong-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Note 20 5G',
+        price: '32.990.000',
+        img: './assets/img/product/phone/samsung/samsunggalaxynote20ultratrangnew-600x600-600x600.jpg'
+    },
+];
+
+export const tablet_Samsung = [
+    {
+        product_name: 'Samsung Galaxy Tab S7',
+        price: '18.590.000',
+        img: './assets/img/product/tablet/samsung/samsung-galaxy-tab-s7-gold-new-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Tab A7',
+        price: '7.990.000',
+        img: './assets/img/product/tablet/samsung/samsung-galaxy-tab-a7-2020-vangdong-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Tab A8',
+        price: '3.690.000',
+        img: './assets/img/product/tablet/samsung/samsung-galaxy-tab-a8-t295-2019-silver-1-600x600.jpg'
+    },
+    {
+        product_name: 'Samsung Galaxy Tab S6 Lite',
+        price: '9.990.000',
+        img: './assets/img/product/tablet/samsung/samsung-galaxy-tab-s6-lite-600x600-2-600x600.jpg'
+    },
+];
+
+export const iPad = [
+    {
+        product_name: 'iPad Ari 4 Wifi Cellular 256GB (2020)',
+        price: '24.190.000',
+        img: './assets/img/product/tablet/iPad/ipad-4-cellular-xanh-new-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'iPad Ari 4 Wifi 64GB (2020)',
+        price: '16.190.000',
+        img: './assets/img/product/tablet/iPad/ipad-air-4-wifi-64gb-2020-xanhla-600x600-600x600.jpg'
+    },
+    {
+        product_name: 'iPad Pro 12.9inch Wifi Cellular 128GB (2020)',
+        price: '31.190.000',
+        img: './assets/img/product/tablet/iPad/ipad-pro-12-9-inch-wifi-128gb-2020-bac-600x600-1-600x600.jpg'
+    },
+    {
+        product_name: 'iPad Pro 12.9inch Wifi Cellular 128GB (2020)',
+        price: '27.590.000',
+        img: './assets/img/product/tablet/iPad/ipad-pro-12-9-inch-wifi-cellular-128gb-2020-bac-600x600-1-600x600.jpg'
+    },
+    {
+        product_name: 'iPad Pro M1 Wifi Cellular 256GB (2021)',
+        price: '37.990.000',
+        img: './assets/img/product/tablet/iPad/ipad-pro-2021-129-inch-silver-600x600.jpg'
+    },
+    {
+        product_name: 'iPad Pro M1 Wifi Cellular 128GB (2021)',
+        price: '29.590.000',
+        img: './assets/img/product/tablet/iPad/ipad-pro-2021-129-inch-gray-600x600.jpg'
+    },
+
+];
+
+export const sacDienThoai = [
+    {
+        product_name: 'Sạc Super VOOC 30W OPPO',
+        price: '740.000',
+        img: './assets/img/product/accessories/sacDienThoai/vooc-30w-oppo-vc56jauh-new-600x600.jpg'
+    },
+    {
+        product_name: 'Sạc nhanh Samsung 25W ',
+        price: '490.000',
+        img: './assets/img/product/accessories/sacDienThoai/type-c-pd-25w-samsung-ep-ta800nw-trang-600x600.jpg'
+    },
+    {
+        product_name: 'Sạc Super VOOC 65W OPPO',
+        price: '990.000',
+        img: './assets/img/product/accessories/sacDienThoai/supervooc-65w-oppo-vca7jauh-new-600x600.jpg'
+    },
+    {
+        product_name: 'Cáp Type C Hydrus',
+        price: '54.000',
+        img: './assets/img/product/accessories/sacDienThoai/cap-type-c-1m-hydrus-dr-t01-ava-1-600x600.jpg'
+    },
+    {
+        product_name: 'Cáp Lightning Hydrus',
+        price: '54.000',
+        img: './assets/img/product/accessories/sacDienThoai/cap-lightning-1m-hydrus-dr-l03-ava-600x600.jpg'
+    },
+    {
+        product_name: 'Cáp chuyển đổi Type C - 3.5mm',
+        price: '180.000',
+        img: './assets/img/product/accessories/sacDienThoai/cap-chuyen-doi-type-c-sang-35mm-samsung-ee-uc10juw-600x600.jpg'
+    },
+];
+
+export const  pinDuPhong = [
+    {
+        product_name: 'Pin sạc dự phòng Hydrus 10.000mAh',
+        price: '343.000',
+        img: './assets/img/product/accessories/pinDuPhong/hydrus-pj-jp196-ava-600x600.jpg'
+    },
+    {
+        product_name: 'Pin sạc dự phòng Oppo VOOC 10.000mAh',
+        price: '1.800.000',
+        img: './assets/img/product/accessories/pinDuPhong/oppo-vooc-30w-pbv02-1-600x600.jpg'
+    },
+    {
+        product_name: 'Pin sạc dự phòng Samsung 10.000mAh',
+        price: '990.000',
+        img: './assets/img/product/accessories/pinDuPhong/polymer-10000mah-type-c-25w-samsung-eb-p3300-600x600.jpg'
+    },
+    {
+        product_name: 'Pin sạc dự phòng Xiaomi 10.000mAh',
+        price: '690.000',
+        img: './assets/img/product/accessories/pinDuPhong/polymer-10000mah-xiaomi-ultra-compact-avt-1-600x600.jpg'
+    },
+    // {
+    //     product_name: 'Cáp Lightning Hydrus',
+    //     price: '54.000',
+    //     img: './assets/img/product/accessories/pinDuPhong/cap-lightning-1m-hydrus-dr-l03-ava-600x600.jpg'
+    // },
+    // {
+    //     product_name: 'Cáp chuyển đổi Type C - 3.5mm',
+    //     price: '180.000',
+    //     img: './assets/img/product/accessories/pinDuPhong/cap-chuyen-doi-type-c-sang-35mm-samsung-ee-uc10juw-600x600.jpg'
+    // },
+];
---
+++ assets/js/nav_index.js
@@ -0,0 +1,29 @@
+// // import {iphone} from './content.js'
+// // imported.src = './content.js'
+
+// // console.log(imported)
+// // let pho = iphone;
+
+// $(document).ready(function () {
+
+//     $.getScript('data.js', function() {
+//         //script is loaded and executed put your dependent JS here
+    
+//         $("#iphone").click(function () {
+//             console.log(Iphone[0].name)
+        
+//         })
+//     });
+
+//     $("#phone").click(function () {
+//         // console.log("phone")
+//         for (let i = 0; i < iphone.length; i++) {
+    
+//             console.log(iphone[i].name)
+            
+//         }
+//     })
+//     $("#iphone").click(function () {
+//         console.log("phone")
+//     })
+// })
